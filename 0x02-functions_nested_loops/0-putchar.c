d5b349d931aedec0e36ac3337bb9a13b2f3e0532
Mozamane <mozamanebaloyi1994@gmail.com>
2022-02-08 13:53:06 +0200

    a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "holberton.h"
 /**
- * main - Entry point
+ * main - Prints Holberton
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char *sh = "_putchar";
-
-        while (*sh)
-        {
-                putchar(*sh);
-                sh++;
-                putchar('\n');
-        }
-
-        return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
