bb2339067afeeaa18bbc8edd3e59a2bb23b752f3
Mozamane <61825007+Mozamane@users.noreply.github.com>
2022-07-21 11:59:53 +0200

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 char *_strcat(char *dest, char *src);
+void reset_to_98(int *n);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
